3ea7ad1f39b3a8cbfccba818efa7089e98e1b6b5
yacklebeam <jacob@jmtroxel.io>
2019-09-25 16:51:18 -0700

    switching to raylib for LD45

--- main.go
+++ main.go
@@ -1,43 +1,21 @@
 package main
 
-import (
-	"github.com/veandco/go-sdl2/sdl"
-)
-
-const (
-	gWindowHeight = 600
-	gWindowWidth  = 800
-)
+import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		panic(err)
-	}
-	defer sdl.Quit()
+	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 
-	window, err := sdl.CreateWindow("ld45 test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, gWindowWidth, gWindowHeight, sdl.WINDOW_SHOWN)
-	if err != nil {
-		panic(err)
-	}
-	defer window.Destroy()
+	rl.SetTargetFPS(60)
 
-	surface, err := window.GetSurface()
-	if err != nil {
-		panic(err)
-	}
-	surface.FillRect(nil, 0)
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
 
-	rect := sdl.Rect{X: 0, Y: 0, W: gWindowWidth, H: gWindowHeight}
-	surface.FillRect(&rect, 0xff0000) // RGB format
-	window.UpdateSurface()
+		rl.ClearBackground(rl.RayWhite)
 
-	running := true
-	for running {
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
-			case *sdl.QuitEvent:
-				running = false
-			}
-		}
+		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+		rl.EndDrawing()
 	}
+
+	rl.CloseWindow()
 }
