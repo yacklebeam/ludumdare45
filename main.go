7b5d6c1c549baf1da3aa5abe06643c316d31b1f2
yacklebeam <jacob@jmtroxel.io>
2019-09-25 17:05:59 -0700

    Updated example for our purposes

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+const (
+	gWindowWidth  = 800
+	gWindowHeight = 600
+)
+
 func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	rl.InitWindow(gWindowWidth, gWindowHeight, "Ludum Dare #45 Game")
+
+	textColor := rl.Gray
 
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
+
+		if rl.IsKeyDown(rl.KeyQ) {
+			textColor = rl.Red
+		}
+
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
 
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+		rl.DrawText("LUDUM DARE 45 GAME", 190, 200, 20, textColor)
 
 		rl.EndDrawing()
 	}
