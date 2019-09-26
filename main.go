146a8c530cf6de126af3d0bb5cc0c5089c601ef1
yacklebeam <jacob@jmtroxel.io>
2019-09-25 19:33:06 -0700

    menu testing

--- main.go
+++ main.go
@@ -7,24 +7,56 @@ const (
 	gWindowHeight = 600
 )
 
+type menu struct {
+	buttons []button
+	x       int32
+	y       int32
+}
+
+type button struct {
+	text string
+}
+
+func intersect(a, b rl.RectangleInt32) bool {
+	if a.X+a.Width < b.X || a.Y+a.Height < b.Y || a.X > b.X+b.Width || a.Y > b.Y+b.Height {
+		return false
+	}
+	return true
+}
+
 func main() {
 	rl.InitWindow(gWindowWidth, gWindowHeight, "Ludum Dare #45 Game")
 
-	textColor := rl.Gray
+	m := menu{x: (gWindowWidth / 2) - 100, y: 200}
+	m.buttons = append(m.buttons, button{text: "Option 1"})
+	m.buttons = append(m.buttons, button{text: "Option 2"})
+	m.buttons = append(m.buttons, button{text: "Option 3"})
 
-	rl.SetTargetFPS(60)
+	//rl.SetTargetFPS(200)
 
 	for !rl.WindowShouldClose() {
-
-		if rl.IsKeyDown(rl.KeyQ) {
-			textColor = rl.Red
-		}
-
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
 
-		rl.DrawText("LUDUM DARE 45 GAME", 190, 200, 20, textColor)
+		mouseX := rl.GetMouseX()
+		mouseY := rl.GetMouseY()
+
+		for i, b := range m.buttons {
+			buttonColor := rl.Red
+			margin := 10
+			buttonWidth := 200
+			buttonHeight := 50
+
+			if intersect(rl.RectangleInt32{X: m.x, Y: m.y + int32(60*i), Width: 200, Height: 50}, rl.RectangleInt32{X: mouseX, Y: mouseY, Width: 0, Height: 0}) {
+				buttonColor = rl.Blue
+				buttonWidth = 210
+				buttonHeight = 50
+			}
+
+			rl.DrawRectangle(m.x, m.y+int32((margin+buttonHeight)*i), int32(buttonWidth), int32(buttonHeight), buttonColor)
+			rl.DrawText(b.text, m.x+10, m.y+int32(60*i)+10, 30, rl.White)
+		}
 
 		rl.EndDrawing()
 	}
