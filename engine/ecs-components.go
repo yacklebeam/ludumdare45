46e55924ffeb841baae18c237b861c44f1b424d8
yacklebeam <jacob@jmtroxel.io>
2019-10-04 16:16:50 -0700

    added player, render, position components

--- engine/ecs-components.go
+++ engine/ecs-components.go
@@ -1,1 +1,20 @@
 package engine
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type playerCoSingleton struct {
+	currentAccountValue int64
+}
+
+type renderCo struct {
+	texture    string
+	tint       rl.Color
+	sourceRect rl.Rectangle
+}
+
+type positionCo struct {
+	x float32
+	y float32
+}
