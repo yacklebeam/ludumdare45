9826057dff09ad4c95bf93c20870dbdc4159e2ae
yacklebeam <jacob@jmtroxel.io>
2019-10-04 14:01:32 -0700

    added system and assets folders

---
+++ system/system.go
@@ -0,0 +1,12 @@
+package system
+
+// system code is NOT ECS systems
+// system is for interfacing with assets, etc
+// - texture loading
+// - audio loading
+// - game data loading
+
+func loadTextureFromFile(filename string) {
+	// always loads from assets/images
+
+}
