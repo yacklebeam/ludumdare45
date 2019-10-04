072d16746f96190be47a6279d3995f8b28559225
yacklebeam <jacob@jmtroxel.io>
2019-10-04 14:08:06 -0700

    added loading of audio and textures

--- system/system.go
+++ system/system.go
@@ -1,12 +1,32 @@
 package system
 
+import rl "github.com/gen2brain/raylib-go/raylib"
+
 // system code is NOT ECS systems
 // system is for interfacing with assets, etc
 // - texture loading
 // - audio loading
 // - game data loading
 
+var textureMap map[string]rl.Texture2D
+var audioMap map[string]rl.Sound
+
+func init() {
+	textureMap = make(map[string]rl.Texture2D)
+	audioMap = make(map[string]rl.Sound)
+}
+
 func loadTextureFromFile(filename string) {
 	// always loads from assets/images
+	// this function MUST be run after rl.InitWindow() in the main game
+	img := rl.LoadImage("assets/images/" + filename)
+	texture := rl.LoadTextureFromImage(img)
+	textureMap[filename] = texture
+}
 
+func loadAudioFromFile(filename string) {
+	// always loads from assets/audio
+	// this function MUST be run after rl.InitWindow() in the main game
+	sound := rl.LoadSound("assets/audio/" + filename)
+	audioMap[filename] = sound
 }
